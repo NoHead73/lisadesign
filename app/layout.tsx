2e0d0a659f62b8d9941bb0ac744c9528d98f72e1
NoHead73 <alenin73@mail.ru>
2026-06-03 18:53:51 +0300

    Исправлен импорт AOS через globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,37 @@
-import type { Metadata } from "next";
+﻿import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "aos/dist/aos.css";
 import Preloader from "./components/Preloader";
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "LisaDesign",
-    description: "Профессиональный дизайн и разработка сайтов",
+  title: "LisaDesign",
+  description: "Профессиональный дизайн и разработка сайтов",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="ru">
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+  return (
+    <html lang="ru">
+      <body
+        className={${geistSans.variable}  antialiased}
+      >
         <Preloader>
-            {children}
+          {children}
         </Preloader>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
